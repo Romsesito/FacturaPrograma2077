f5f74da28a682121482d05f92b3f1b7ae141c0c8
Romsesito <graphicsrom@gmail.com>
2023-05-30 17:39:30 -0500

    V0.2

---
+++ src/LOGO.java
@@ -0,0 +1,13 @@
+public class LOGO {
+
+    public void printLogo() {
+        System.out.println(" __     __                  _    _____   _                     ");
+        System.out.println(" \\ \\   / /                 (_)  / ____| | |                    ");
+        System.out.println("  \\ \\_/ /   _   _   _ __    _  | (___   | |__     ___    _ __  ");
+        System.out.println("   \\   /   | | | | | '_ \\  | |  \\___ \\  | '_ \\   / _ \\  | '_ \\ ");
+        System.out.println("    | |    | |_| | | |_) | | |  ____) | | | | | | (_) | | |_) |");
+        System.out.println("    |_|     \\__,_| | .__/  |_| |_____/  |_| |_|  \\___/  | .__/ ");
+        System.out.println("                   | |                                  | |    ");
+        System.out.println("                   |_|                                  |_|    ");
+    }
+}
